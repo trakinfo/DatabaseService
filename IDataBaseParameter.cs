36e94b227c947bf9efc8cc9b68d07048108da5df
Akacja <tomasz.traczyk@onet.eu>
2019-06-01 19:53:03 +0200

    IDataBaseParameter interface and DataBaseParameter class were added.

---
+++ IDataBaseParameter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DataBaseService
+{
+    public interface IDataBaseParameter
+    {
+        string Name { get; set; }
+        IEnumerable<object> Value { get; set; }
+    }
+}
